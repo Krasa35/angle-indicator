6beead79fa8c4a1964a4da42fb8173cefa5bc9f0
Kurus <Kurus.Kurus@wp.pl>
2024-01-12 21:05:17 +0100

    First STM32 project f7

---
+++ 01_STM/Debug/Components/Src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Components/Src/AS5600.c 
+
+OBJS += \
+./Components/Src/AS5600.o 
+
+C_DEPS += \
+./Components/Src/AS5600.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Components/Src/%.o Components/Src/%.su Components/Src/%.cyclo: ../Components/Src/%.c Components/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F746xx -c -I../Core/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F7xx/Include -I../Drivers/CMSIS/Include -I"C:/Users/Kurus/Documents/github z krasa/inverted-pendulum/01_STM/Components/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Components-2f-Src
+
+clean-Components-2f-Src:
+	-$(RM) ./Components/Src/AS5600.cyclo ./Components/Src/AS5600.d ./Components/Src/AS5600.o ./Components/Src/AS5600.su
+
+.PHONY: clean-Components-2f-Src
+
--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-#   Inverted Pendulum
-Project of regulation system for angle indicator based on STM32. Software is developed in STM32CUBEIDE. Based on Doxygen documentation and with report written under link in [Overleaf](https://www.overleaf.com/read/czjpmjzbdcst#1883af) 
-##  Genesis
+# Inverted Pendulum
+
+Project of regulation system for angle indicator based on STM32. Software is developed in STM32CUBEIDE. Based on Doxygen documentation and with report written under link in [Overleaf](https://www.overleaf.com/read/czjpmjzbdcst#1883af)
+
+## Genesis
+
 Project orginally intended to be regulation system for inverted pendulum but after considerations and thoughts it was decided to create angle indicator object, so name was kept because of first willingnesses.
 
-#   Changelog
-*   2024-01-07 -- 0.0.0
-    *   Startup of git repo and files for first report - intended project inside **99_Docs**
+# Changelog
+
+- 2024-01-07 -- 0.0.0
+  - Startup of git repo and files for first report - intended project inside **99_Docs**
+- 2024-01-12 -- 0.0.1
+
+  - First STM32 project f7
+
+    ![alt](99_Docs\98_Photos\Graph_001.png)
